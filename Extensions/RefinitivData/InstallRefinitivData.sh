f5410175584cacb64977bfd0aa2364717d8053b6
Rob Lundie Hill <rob.lundiehill@thomsonreuters.com>
2018-12-13 14:48:58 +0000

    improved install script

--- Extensions/RefinitivData/InstallRefinitivData.sh
+++ Extensions/RefinitivData/InstallRefinitivData.sh
@@ -1,8 +1,15 @@
-/anaconda/bin/pip install google-cloud-bigquery
+/anaconda/bin/pip install google-cloud-bigquery==1.4.0
+/anaconda/bin/pip install elasticsearch==6.3.1
 
+cd /home
+cd *
 
-mkdir -p /notebooks
-cd /notebooks
+mkdir -p notebooks
+chown 1003:1003 notebooks
+cd notebooks
+
+mkdir -p notebooks/Refinitiv
+cd Refinitiv
 
 curl -v -O 'https://dsa-'$DSA_ENV'-ecs-api.fr-nonprod.aws.thomsonreuters.com/v1/examples/'$DSA_PERMID'/data-science-accelerator-examples.zip' \
     -H 'Accept: application/json, text/plain, */*' \
@@ -21,3 +28,10 @@ if [ "$(cat status_code)" != "200" ]; then
 fi
 
 unzip data-science-accelerator-examples.zip
+
+mv data-science-accelerator-examples/notebooks .
+
+rm -rf data-science-accelerator-examples.zip data-science-accelerator-examples 
+
+cd ..
+chown -R 1003:1003 Refinitiv
